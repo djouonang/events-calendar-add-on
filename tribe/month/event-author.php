fd248df329605f691196e92a85044bfbe8e8c76b
Djouonang Landry <djouonanglandry@yahoo.com>
2022-03-04 16:53:57 +0100

    Add files via upload

---
+++ tribe/month/event-author.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Month View Single Day
+ * This file contains one day in the month grid
+ *
+ * Override this template in your own theme by creating a file at [your-theme]/tribe-events/month/single-day.php
+ *
+ * @package TribeEventsCalendar
+ * @since  3.0
+ * @author Modern Tribe Inc.
+ *
+ */
+
+if ( !defined('ABSPATH') ) { die('-1'); } ?>
+<div class="tribe-common-b2">
+ Author: <?php the_author(); ?>
+</div>
